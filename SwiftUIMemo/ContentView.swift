d400c25563dace3934eafc0a6b153f7aa13abf5a
nayoung <ny2322@gmail.com>
2021-04-10 01:22:49 +0900

    Initial Commit

---
+++ SwiftUIMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by Nayomi on 4/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
