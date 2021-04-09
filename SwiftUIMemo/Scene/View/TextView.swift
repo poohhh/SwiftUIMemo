6a002d9b506410df20676dd8f362171803233716
nayoung <ny2322@gmail.com>
2021-04-10 02:49:11 +0900

    text view

---
+++ SwiftUIMemo/Scene/View/TextView.swift
@@ -0,0 +1,40 @@
+//
+//  TextView.swift
+//  SwiftUIMemo
+//
+//  Created by Nayomi on 4/10/21.
+//
+
+import UIKit
+import SwiftUI
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UITextView {
+        let myTextview = UITextView()
+        myTextview.delegate = context.coordinator
+        
+        return myTextview
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: TextView
+        
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+        
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
+    }
+}
