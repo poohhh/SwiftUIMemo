719e5ad07361bedeefdcc2a15568f45c566718ee
nayoung <ny2322@gmail.com>
2021-04-10 01:41:23 +0900

    model class

---
+++ SwiftUIMemo/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by Nayomi on 4/10/21.
+//
+
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(id: UUID = UUID(), content: String, insertDate: Date = Date()) {
+        self.id = id
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
+
+extension Memo: Equatable {
+    static func == (lhs: Memo, rhs: Memo) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
