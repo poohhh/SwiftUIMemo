71d3ddce730e50aba9f208913612571750e920c7
nayoung <ny2322@gmail.com>
2021-04-10 03:54:01 +0900

    detail scene

---
+++ SwiftUIMemo/Scene/DetailScene.swift
@@ -0,0 +1,44 @@
+//
+//  DetailScene.swift
+//  SwiftUIMemo
+//
+//  Created by Nayomi on 4/10/21.
+//
+
+import SwiftUI
+
+struct DetailScene: View {
+    @ObservedObject var memo: Memo
+    
+    @EnvironmentObject var store: MemoStore
+    @EnvironmentObject var formatter: DateFormatter
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(self.memo.content)
+                            .padding()
+                        
+                        Spacer()
+                    }
+                    
+                    Text("\(self.memo.insertDate, formatter: formatter)")
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                }
+            }
+        }
+        .navigationBarTitle("메모 보기")
+    }
+}
+
+struct DetailScene_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScene(memo: Memo(content: "SwiftUI"))
+            .environmentObject(MemoStore())
+            .environmentObject(DateFormatter.memoDateFormatter)
+    }
+}
